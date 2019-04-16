d7f478ceef1a310da5cb7f3028c9ac358f7f3fd6
Charlyn Manuyag <charlyn.manuyag@berkeley.edu>
2019-04-16 15:33:39 -0400

    Added the functional component for ItemView

---
+++ client/components/ItemView.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const ItemView = props => {
+  const {product, handleClick} = props
+  return (
+    <div className="itemViewContainer">
+      <div className="itemImage">
+        <img src={product.imageUrl} />
+      </div>
+      <div className="itemName">{product.name}</div>
+      <div className="itemPrice">{product.price}</div>
+      <div className="addToCart">
+        <button type="button" onClick={handleClick}>
+          Add to cart
+        </button>
+      </div>
+    </div>
+  )
+}
