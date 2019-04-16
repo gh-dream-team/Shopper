6b21ad8ffbc321a4be2e328ddc25cb4dab0ef42f
Linda Saraguro <lindasaraguro@yahoo.com>
2019-04-16 17:02:58 -0400

    updated signup

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,10 +3,20 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: false
+    }
+  },
   email: {
     type: Sequelize.STRING,
     unique: true,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
   },
   password: {
     type: Sequelize.STRING,
