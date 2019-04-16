a2a74da8993f6a016fd3a5c0fc11cd3a816d7c86
Charlyn Manuyag <charlyn.manuyag@berkeley.edu>
2019-04-16 16:12:46 -0400

    added dispatch on Item View

--- client/components/ItemView.js
+++ client/components/ItemView.js
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {addProduct} from '../store/cart.js'
+import {connect} from 'react-redux'
 
-export const ItemView = props => {
-  const {product, handleClick} = props
-  return (
-    <div className="itemViewContainer">
-      <div className="itemImage">
-        <img src={product.imageUrl} />
+export class ItemView extends Component {
+  render() {
+    const {product} = this.props
+    return (
+      <div className="itemViewContainer">
+        <div className="itemImage">
+          <img src={product.imageUrl} />
+        </div>
+        <div className="itemName">{product.name}</div>
+        <div className="itemPrice">{product.price}</div>
+        <div className="addToCart">
+          <button type="button" onClick={this.addProduct(product.id)}>
+            Add to cart
+          </button>
+        </div>
       </div>
-      <div className="itemName">{product.name}</div>
-      <div className="itemPrice">{product.price}</div>
-      <div className="addToCart">
-        <button type="button" onClick={handleClick}>
-          Add to cart
-        </button>
-      </div>
-    </div>
-  )
+    )
+  }
 }
+
+const mapDispatch = dispatch => ({
+  addProduct: id => dispatch(addProduct(id))
+})
+
+export default connect(null, mapDispatch)(ItemView)
