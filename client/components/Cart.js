67baa2403147eb2867221ba0f870784af53deeb5
Jenny Grange <jenniferhelengrange@gmail.com>
2019-04-17 16:47:12 -0400

    adds total

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -3,24 +3,36 @@ import {connect} from 'react-redux'
 import CartItemView from './CartItemView'
 
 class Cart extends Component {
-    //need to add a comp-did-mount
-    //add a dispatch func to get call from cart 
+  constructor() {
+    super()
+    this.state = {
+      total: 0
+    }
+  }
+  //need to add a comp-did-mount
+  //add a dispatch func to get call from cart
 
   render() {
-    const {cart} = this.props
-    console.log("ITEMS", cart)
-    console.log("PROPS",this.props)
+    const {cart, total} = this.props
+    console.log('CART', cart)
     return (
-    <div>
-    <h1>WELCOME TO CART</h1>
-    {cart.map(item => (
-      <CartItemView key={item.id} product={item} />
-    ))}
-    </div>)
+      <div>
+        <h1>WELCOME TO CART</h1>
+        {cart.map(item => (
+          <div>
+            <CartItemView key={item.id} product={item} />
+          </div>
+        ))}
+        <p>Total: ${total}</p>
+      </div>
+    )
   }
 }
 
 const mapState = state => ({
+  total: state.cart[0]
+    ? state.cart.reduce((total, item) => total + item.price * item.quantity, 0)
+    : 0,
   cart: state.cart
 })
 
