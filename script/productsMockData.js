dcb8eff0ff17d81cab27dd07dcc8d11e00ac6715
Jenny Grange <jenniferhelengrange@gmail.com>
2019-04-18 11:38:19 -0400

    adds validations to productModel and removes quantity field

--- script/productsMockData.js
+++ script/productsMockData.js
@@ -4,8 +4,8 @@ const products = [
     name: 'Yams',
     description:
       'Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh. In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo.',
-    imageUrl: 'http://dummyimage.com/201x131.bmp/dddddd/000000',
-    price: 30.29,
+
+    price: 3029,
     inventory: 50
   },
   {
@@ -14,7 +14,7 @@ const products = [
     description:
       'Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede. Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.',
     imageUrl: 'http://dummyimage.com/179x148.bmp/dddddd/000000',
-    price: 18.93,
+    price: 1893,
     inventory: 21
   },
   {
@@ -23,7 +23,7 @@ const products = [
     description:
       'Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.',
     imageUrl: 'http://dummyimage.com/231x175.png/cc0000/ffffff',
-    price: 31.32,
+    price: 3132,
     inventory: 56
   },
   {
@@ -32,7 +32,7 @@ const products = [
     description:
       'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat.',
     imageUrl: 'http://dummyimage.com/236x174.png/5fa2dd/ffffff',
-    price: 57.83,
+    price: 5783,
     inventory: 9
   },
   {
@@ -41,7 +41,7 @@ const products = [
     description:
       'Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.',
     imageUrl: 'http://dummyimage.com/161x125.bmp/cc0000/ffffff',
-    price: 51.9,
+    price: 519,
     inventory: 88
   },
   {
@@ -50,7 +50,7 @@ const products = [
     description:
       'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc.',
     imageUrl: 'http://dummyimage.com/117x151.jpg/cc0000/ffffff',
-    price: 27.0,
+    price: 270,
     inventory: 11
   },
   {
@@ -58,7 +58,7 @@ const products = [
     name: 'Eggplant Italian',
     description: 'In congue. Etiam justo.',
     imageUrl: 'http://dummyimage.com/143x231.png/5fa2dd/ffffff',
-    price: 73.48,
+    price: 7348,
     inventory: 10
   },
   {
@@ -67,7 +67,7 @@ const products = [
     description:
       'Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque. Duis bibendum. Morbi non quam nec dui luctus rutrum.',
     imageUrl: 'http://dummyimage.com/219x213.png/dddddd/000000',
-    price: 67.47,
+    price: 6747,
     inventory: 72
   },
   {
@@ -76,7 +76,7 @@ const products = [
     description:
       'Duis mattis egestas metus. Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh. Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique.',
     imageUrl: 'http://dummyimage.com/143x152.png/cc0000/ffffff',
-    price: 48.73,
+    price: 4873,
     inventory: 11
   },
   {
@@ -85,7 +85,7 @@ const products = [
     description:
       'Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor. Duis mattis egestas metus.',
     imageUrl: 'http://dummyimage.com/228x214.jpg/ff4444/ffffff',
-    price: 65.38,
+    price: 6538,
     inventory: 58
   },
   {
@@ -94,7 +94,7 @@ const products = [
     description:
       'Integer ac neque. Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus. In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus. Suspendisse potenti.',
     imageUrl: 'http://dummyimage.com/134x237.png/ff4444/ffffff',
-    price: 51.2,
+    price: 512,
     inventory: 84
   },
   {
@@ -103,7 +103,7 @@ const products = [
     description:
       'Donec dapibus. Duis at velit eu est congue elementum. In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.',
     imageUrl: 'http://dummyimage.com/192x107.jpg/5fa2dd/ffffff',
-    price: 45.84,
+    price: 4584,
     inventory: 80
   },
   {
@@ -112,7 +112,7 @@ const products = [
     description:
       'Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor. Duis mattis egestas metus. Aenean fermentum. Donec ut mauris eget massa tempor convallis.',
     imageUrl: 'http://dummyimage.com/166x194.jpg/5fa2dd/ffffff',
-    price: 34.22,
+    price: 3422,
     inventory: 86
   },
   {
@@ -121,7 +121,7 @@ const products = [
     description:
       'Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc.',
     imageUrl: 'http://dummyimage.com/165x198.jpg/dddddd/000000',
-    price: 21.24,
+    price: 2124,
     inventory: 62
   },
   {
@@ -130,7 +130,7 @@ const products = [
     description:
       'Aliquam non mauris. Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis. Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem. Sed sagittis.',
     imageUrl: 'http://dummyimage.com/157x123.png/ff4444/ffffff',
-    price: 51.58,
+    price: 5158,
     inventory: 92
   },
   {
@@ -139,7 +139,7 @@ const products = [
     description:
       'Donec dapibus. Duis at velit eu est congue elementum. In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros.',
     imageUrl: 'http://dummyimage.com/192x208.png/5fa2dd/ffffff',
-    price: 69.04,
+    price: 6904,
     inventory: 24
   },
   {
@@ -148,7 +148,7 @@ const products = [
     description:
       'Nullam varius. Nulla facilisi. Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit.',
     imageUrl: 'http://dummyimage.com/241x204.png/dddddd/000000',
-    price: 44.2,
+    price: 442,
     inventory: 80
   },
   {
@@ -157,7 +157,7 @@ const products = [
     description:
       'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl. Aenean lectus.',
     imageUrl: 'http://dummyimage.com/113x117.jpg/5fa2dd/ffffff',
-    price: 86.1,
+    price: 861,
     inventory: 32
   },
   {
@@ -166,7 +166,7 @@ const products = [
     description:
       'Suspendisse potenti. Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
     imageUrl: 'http://dummyimage.com/138x206.png/dddddd/000000',
-    price: 46.83,
+    price: 4683,
     inventory: 93
   },
   {
@@ -175,7 +175,7 @@ const products = [
     description:
       'Suspendisse potenti. Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia.',
     imageUrl: 'http://dummyimage.com/178x176.jpg/5fa2dd/ffffff',
-    price: 38.49,
+    price: 3849,
     inventory: 90
   }
 ]
--- server/db/models/Product.js
+++ server/db/models/Product.js
@@ -3,23 +3,33 @@ const db = require('../db')
 
 const Product = db.define('products', {
   name: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false
   },
   description: {
     type: Sequelize.TEXT
   },
   imageUrl: {
-    type: Sequelize.TEXT
+    type: Sequelize.TEXT,
+    defaultValue:
+      'https://d31fr2pwly4c4s.cloudfront.net/1/2/1/1026291_1_i-love-the-90s-vs-00s_400.jpg',
+    validate: {
+      isURL: true
+    }
   },
   price: {
-    type: Sequelize.DECIMAL
-  },
-  quantity: {
     type: Sequelize.INTEGER,
-    defaultValue: 1
+    allowNull: false,
+    validate: {
+      isInt: true,
+      min: 0
+    }
   },
   inventory: {
-    type: Sequelize.INTEGER
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 0
+    }
   }
 })
 
