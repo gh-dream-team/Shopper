33522ce7a3417855da1631507694e679c6e91289
Charlyn Manuyag <charlyn.manuyag@berkeley.edu>
2019-04-18 11:53:28 -0400

    trying to do post route

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {Cart, User} = require('../db/models/index.js')
+const {Cart, User, Product, Item} = require('../db/models/index.js')
 
 router.get('/', async (req, res, next) => {
   try {
@@ -14,6 +14,20 @@ router.get(':/userId', async (req, res, next) => {
   try {
     const id = req.params.userId
     const cart = await Cart.findAll({where: {userId: id, isPurchased: false}})
+    res.json(cart)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//We want to initialize a new user with a new cart with an item
+router.post('/', async (req, res, next) => {
+  try {
+    const id = req.user.id
+    const cart = await Cart.create(req.body, {userId: id})
+    // const product = await Product.get(req.body.id)
+    // const newItem = await Item.create()
+    // const newCart = await Cart.create(req.body, {where: })
     res.json(cart)
   } catch (error) {
     next(error)
