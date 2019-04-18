db1e506407286dd4fcaffef47dc7cbcb84e5cdcd
Jesse Evans <jesseelizabeth1@gmail.com>
2019-04-18 11:52:00 -0400

    added default value for itemIds

---
+++ server/db/models/Item.spec.js
@@ -0,0 +1,25 @@
+const {expect} = require('chai')
+const db = require('../index')
+const Item = db.model('item')
+
+describe('Item model', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('instanceMethods', () => {
+    describe('correctQuantity', () => {
+      let product
+
+      beforeEach(async () => {
+        product = await Item.create({
+          quantity: 5
+        })
+      })
+
+      it('should return true for a quantity of 5', () => {
+        expect(product.correctQuantity('5')).to.be.equal(true)
+      })
+    }) // end describe('correctPassword')
+  }) // end describe('instanceMethods')
+}) // end describe('User model')
