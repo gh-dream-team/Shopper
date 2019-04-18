f0072079e874499dcf55c231d8104cbacd395d8e
Jesse Evans <jesseelizabeth1@gmail.com>
2019-04-18 15:24:51 -0400

    PART TWOOOOO

--- server/api/cart.js
+++ server/api/cart.js
@@ -4,14 +4,13 @@ const {Cart, Item} = require('../db/models/index.js')
 router.get('/', async (req, res, next) => {
   try {
     const carts = await Cart.findAll()
-    console.log('Im in cart', carts)
     res.json(carts)
   } catch (error) {
     next(error)
   }
 })
 
-router.get(':/userId', async (req, res, next) => {
+router.get('/:userId', async (req, res, next) => {
   try {
     const id = req.params.userId
     const cart = await Cart.findAll({where: {userId: id, isPurchased: false}})
