c37534e4df15195803e957b9cecd11b54c867de3
Jesse Evans <jesseelizabeth1@gmail.com>
2019-04-18 15:35:06 -0400

    fixed signup route

--- server/auth/index.js
+++ server/auth/index.js
@@ -21,10 +21,9 @@ router.post('/login', async (req, res, next) => {
 
 router.post('/signup', async (req, res, next) => {
   try {
-    console.log('BODY', req.body)
     const user = await User.create(req.body)
-    const cart = await Cart.create({userId: user.id})
-    req.login(user, cart, err => (err ? next(err) : res.json(user, cart)))
+    await Cart.create({userId: user.id})
+    req.login(user, err => (err ? next(err) : res.json(user)))
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
       res.status(401).send('User already exists')
