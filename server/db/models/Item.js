4fd53c93dd45ae18b1ac656db4dd14f7d2096f71
Jesse Evans <jesseelizabeth1@gmail.com>
2019-04-18 11:11:43 -0400

    working on tests for item model

--- server/db/models/Item.js
+++ server/db/models/Item.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-const Item = db.define('items', {
+const Item = db.define('item', {
   quantity: {
     type: Sequelize.INTEGER,
     validate: {
