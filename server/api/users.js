998991b4e8563d420970bec57f3aa79128f14d47
Linda Saraguro <lindasaraguro@yahoo.com>
2019-04-23 11:41:59 -0400

    security updates

--- server/api/users.js
+++ server/api/users.js
@@ -1,22 +1,22 @@
-// const router = require('express').Router()
-// const {User} = require('../db/models')
-// module.exports = router
+const router = require('express').Router()
+const {User} = require('../db/models')
+module.exports = router
 
-// router.get('/', async (req, res, next) => {
-//   try {
-//     if(req.user.admin === true) 
-// give access to all users. else send a message notifying the user that they dont have admin access
-// add a admin feild to our user model. which would just be a boolean 
-//     const users = await User.findAll({
-//       // explicitly select only the id and email fields - even though
-//       // users' passwords are encrypted, it won't help if we just
-//       // send everything to anyone who asks!
-//       attributes: ['id', 'email', 'username']
-//     })
-//     res.json(users)
-//   } catch (err) {
-//     next(err)
-//   }
-// })
-
-// in our app do we ever need to list our users? if not, we can delete this entire thing
+router.get('/', async (req, res, next) => {
+  try {
+    if(req.user.admin) {
+      const users = await User.findAll({
+        // explicitly select only the id and email fields - even though
+        // users' passwords are encrypted, it won't help if we just
+        // send everything to anyone who asks!
+        attributes: ['id', 'email', 'username']
+      })
+      res.json(users)
+    }
+    else{
+      res.send('Access denied')
+    }
+  } catch (err) {
+    next(err)
+  }
+})
